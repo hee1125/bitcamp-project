09e4adc0187db8114626038e3ea72cd0cd928701
hee1125 <heeyi1125@nate.com>
2017-11-10 20:25:31 +0900

    '.'

--- bit-springboot01/src/main/java/bigdata3/web/json/ChatBotControl.java
+++ bit-springboot01/src/main/java/bigdata3/web/json/ChatBotControl.java
@@ -3,12 +3,14 @@ package bigdata3.web.json;
 
 import static bigdata3.web.json.JsonResult.*;
 
+import java.util.HashMap;
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController("json.ChatBotControl")
 @RequestMapping("/chatbot/json")
@@ -16,13 +18,20 @@ public class ChatBotControl {
   @Autowired ServletContext application;
   
   @RequestMapping("led/{state}")
-  public Object setLED(@PathVariable String state) throws Exception {
+  public Object setLED(
+      @PathVariable String state, 
+      @RequestParam String senderID) throws Exception {
+    
+    HashMap<String, String> dataMap = new HashMap<>();
+    dataMap.put("senderID", senderID);
+    dataMap.put("state", state);
+    
     if (state.equals("on")) {
-      return new JsonResult(STATE_SUCCESS, state);
+      return new JsonResult(STATE_SUCCESS, dataMap);
     } else if (state.equals("off")) {
-      return new JsonResult(STATE_SUCCESS, state);
+      return new JsonResult(STATE_SUCCESS, dataMap);
     } else {
-      return new JsonResult(STATE_FAIL, state);
+      return new JsonResult(STATE_FAIL, dataMap);
     }
   }
 }
