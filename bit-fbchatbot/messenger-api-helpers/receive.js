b248935a3e54dc5b2e73186e33d8b4fd3b3b145b
hee1125 <heeyi1125@nate.com>
2017-11-27 19:44:24 +0900

    '.'

--- bit-fbchatbot/messenger-api-helpers/receive.js
+++ bit-fbchatbot/messenger-api-helpers/receive.js
@@ -24,7 +24,7 @@ const handleReceiveMessage = (event) => {
             var arr = messageText.split(':')[1].split('=');
             openAPI.searchNewAddress(arr[0], arr[1],(msg)=>{
                 sendAPI.sendTextMessage(senderID, msg);
-            });
+            )};
         } catch (err) {
             console.log(err);
           }
