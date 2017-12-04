e8b3b40c957fce51ec539efa6db1c419517f4a60
unknown <heeyi1125@nate.com>
2017-12-04 20:49:25 +0900

    '.'

--- bit-fbchatbot/messenger-api-helpers/postback-handler.js
+++ bit-fbchatbot/messenger-api-helpers/postback-handler.js
@@ -7,7 +7,7 @@ const postbackHandler = {}
 
 // postback을 처리할 함수를 등록한다.
 const addPostback = (postback, handler) => {
-    postbackHandler[message] = handler;
+    postbackHandler[postback] = handler;
 }
 
 // 등록된 메시지를 핸들러를 찾아서 리턴한다
