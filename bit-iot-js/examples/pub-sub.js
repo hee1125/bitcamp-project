89741b5e8f6ac40ee55934c464eb0dd56ba87c57
unknown <heeyi1125@nate.com>
2017-12-12 17:43:18 +0900

    '.'

--- bit-iot-js/examples/pub-sub.js
+++ bit-iot-js/examples/pub-sub.js
@@ -7,22 +7,22 @@ var awsIot = require('aws-iot-device-sdk');
 // AWS IoT 서버에 등록된 Thing 정보를 바탕으로 장비를 준비시킨다.
 var device = awsIot.device({
     /* AWS 서버에 Thing을 생성한 후 만든 인증서의 개인키 파일*/
-    keyPath: "dev01.private.key", 
+    keyPath: "dev01.private.key",
 
     /* AWS 서버에 Thing을 생성한 후 만든 인증서의 사물인증서 파일*/
     certPath: "dev01.cert.pem",
 
-    /* 사물에 대해 발행한 인증서를 검증해 줄 
+    /* 사물에 대해 발행한 인증서를 검증해 줄
        "인증서를 발행한 회사"의 인증서 파일*/
-    caPath: "root-CA.crt", 
-   
-    /* 다른 클라이언트와 구분하기 위한 임의의 ID */
-    clientId: "client1", 
+    caPath: "root-CA.crt",
 
-    /* AWS에 등록한 Thing을 가리키는 URL. 
-       AWS IoT 사물 관리 페이지에서 "상호작용" 메뉴에서 
+    /* 다른 클라이언트와 구분하기 위한 임의의 ID */
+    clientId: "client1",
+
+    /* AWS에 등록한 Thing을 가리키는 URL.
+       AWS IoT 사물 관리 페이지에서 "상호작용" 메뉴에서
        HTTPS의 RestAPI를 요청할 때 사용할 Thing의 URL이다.*/
-    host: "a222gw6ygk2ekk.iot.ap-northeast-2.amazonaws.com" 
+    host: "a3ag6xqca3ze3x.iot.ap-northeast-2.amazonaws.com"
  });
 
 // 이 프로그램이 AWS IoT에 등록한 Thing과 연결되었을 때 호출될 메서드 추가
@@ -31,13 +31,13 @@ device.on('connect', function() {
     console.log('connect');
 
     // 연결되면 'topic_1'이라는 사서함을 구독하겠다고 선언한다.
-    // => 즉 지금부터 연결된 Thing의 'topic_1'이라는 사서함에 
+    // => 즉 지금부터 연결된 Thing의 'topic_1'이라는 사서함에
     //    메시지가 오면 받겠다는 의미다.
     device.subscribe('topic_1');
     console.log('topic_1의 사서함 구독 시작')
 });
 
-// 구독하기로 설정한 사서함에 메시지가 도착할 때 마다 
+// 구독하기로 설정한 사서함에 메시지가 도착할 때 마다
 // AWS IoT 서버에 이 프로그램에 알려준다.
 // 그때 호출될 메서드를 추가한다.
 device.on('message', function(topic, payload) {
--- bit-iot-py/pub-sub.py
+++ bit-iot-py/pub-sub.py
@@ -1,11 +1,11 @@
-# AWS IoT 파이썬 라이브러리 및 관련 라이브러리 로딩 
+# AWS IoT 파이썬 라이브러리 및 관련 라이브러리 로딩
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import logging
 import time
 import argparse
 import json
 
-# AWS IoT 서버에서 메시지를 받았을 때 호출될 함수 정의 
+# AWS IoT 서버에서 메시지를 받았을 때 호출될 함수 정의
 def customCallback(client, userdata, message):
     print("메시지를 수신하였습니다!")
     print("사서함 이름: ")
@@ -16,22 +16,22 @@ def customCallback(client, userdata, message):
 
 # AWS IoT의 Thing에 접속할 때 사용할 정보 준비
 
-# AWS에 등록한 Thing을 가리키는 URL. 
-# AWS IoT 사물 관리 페이지에서 "상호작용" 메뉴에서 
+# AWS에 등록한 Thing을 가리키는 URL.
+# AWS IoT 사물 관리 페이지에서 "상호작용" 메뉴에서
 # HTTPS의 RestAPI를 요청할 때 사용할 Thing의 URL이다.
-host = "a222gw6ygk2ekk.iot.ap-northeast-2.amazonaws.com"
+host = "a3ag6xqca3ze3x.iot.ap-northeast-2.amazonaws.com"
 
-# 사물에 대해 발행한 인증서를 검증해 줄 
+# 사물에 대해 발행한 인증서를 검증해 줄
 # "인증서를 발행한 회사(인증기관)"의 인증서 파일
 rootCAPath = "root-CA.crt"
 
 # AWS 서버에 Thing을 생성한 후 만든 인증서의 사물인증서 파일
 certificatePath = "dev01.cert.pem"
 
-# AWS 서버에 Thing을 생성한 후 만든 인증서의 개인키 파일 
+# AWS 서버에 Thing을 생성한 후 만든 인증서의 개인키 파일
 privateKeyPath = "dev01.private.key"
 
-# web 소켓 사용 여부 
+# web 소켓 사용 여부
 useWebsocket = False
 
 # 다른 클라이언트와 구분하기 위한 임의의 ID
