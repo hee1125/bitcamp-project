515b466ad3b64086b1c3e086626ed6fdb36f3801
unknown <heeyi1125@nate.com>
2017-12-12 18:50:10 +0900

    '.'

--- bit-iot-py/pub-sub.py
+++ bit-iot-py/pub-sub.py
@@ -11,7 +11,10 @@ def customCallback(client, userdata, message):
     print("사서함 이름: ")
     print(message.topic)
     print("메시지 내용: ")
-    print(message.payload)
+    # print(message.payload)
+    # 사서함에서 받은 JSON 문자열을 객체로 변환
+    dict = json.loads(message.payload)
+    print(dict['message'])
     print("--------------")
 
 # AWS IoT의 Thing에 접속할 때 사용할 정보 준비
