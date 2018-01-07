fd57416ca03482c694ba3761097756627a979776
unknown <heeyi1125@nate.com>
2018-01-07 12:42:11 +0900

    '.'

--- bit-fbchatbot-bang/iot-api/aws.js
+++ bit-fbchatbot-bang/iot-api/aws.js
@@ -59,7 +59,7 @@ dev01.on('message', function(topic, payload) {
     if (payload.toString() == "dht")
         var dht = payload.toString('utf-8');
         var temp = JSON.parse(dht);
-        console.log(dht);
+        console.log(temp);
 
 });
 
