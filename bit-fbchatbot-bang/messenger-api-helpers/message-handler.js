447b1691b41e874be0dc8c13178c63beef70fded
unknown <heeyi1125@nate.com>
2018-01-07 14:05:19 +0900

    '.'

--- bit-fbchatbot-bang/messenger-api-helpers/message-handler.js
+++ bit-fbchatbot-bang/messenger-api-helpers/message-handler.js
@@ -122,21 +122,12 @@ addMessage('메뉴', (recipientId) => {
 
 addMessage('온도', (recipientId, messageText) => {
     //sendAPI.typingOff(recipientId);
-    /*
-    awsIoT.dev01.on('message', function(topic, payload) {
-        console.log('사서함 메시지 도착');
-        console.log('사서함 이름:', topic);
-        console.log('받은 메시지:', payload.toString());
-        console.log('-------------------------');
-        sendAPI.sendTextMessage(recipientId, '현재온도: ' + payload.toString())
-    });
-    */
-    /*
+
     awsIoT.subscribe('dev01', 'topic_1', {
-        var temp = toString(dataObj.temp)
-        sendAPI.sendTextMessage(recipientId, '현재온도: ' + dataObj.temp)
+        var temp = dataObj.temp
+        sendAPI.sendTextMessage(recipientId, '현재온도: ' + temp)
     })
-    */
+
 })
 
 addMessage('습도', (recipientId, messageText) => {
