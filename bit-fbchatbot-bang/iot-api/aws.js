e379a2a3c1e261a266153b1ff6b3958e6cc05596
unknown <heeyi1125@nate.com>
2018-01-07 13:46:25 +0900

    '.'

--- bit-fbchatbot-bang/iot-api/aws.js
+++ bit-fbchatbot-bang/iot-api/aws.js
@@ -53,8 +53,8 @@ dev01.on('message', function(topic, payload) {
     console.log('사서함 이름:', topic);
     var dataObj = payload.toString('utf-8')
     console.log('받은 메시지:', JSON.parse(dataObj));
-    var temp = dataObj.temp
-    console.log('temp');
+    var temp = dataObj.query.temp
+    console.log(temp);
     console.log('-------------------------');
 });
 /*
