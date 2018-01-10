7a31d1fca8fa66bf13534fca5b206f26069795ba
unknown <heeyi1125@nate.com>
2018-01-10 09:37:50 +0900

    '.'

--- bit-iotcenter/src/main/java/bigdata3/service/impl/AwsIotServiceImpl.java
+++ bit-iotcenter/src/main/java/bigdata3/service/impl/AwsIotServiceImpl.java
@@ -8,21 +8,21 @@ import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.iot.client.AWSIotException;
 
-import bigdata3.awsiot.TopicSubscriber;
+import bigdata3.awsiot.TopicPubSub;
 import bigdata3.service.AwsIotService;
 
 @Service
 public class AwsIotServiceImpl implements AwsIotService {
 
   @Autowired
-  TopicSubscriber topicSubscriber;
+  TopicPubSub topicPubSub;
   
   @Override
   public Map<String, Object> getMessage() {
     Map<String,Object> message = new HashMap<>();
-    message.put("humidity", topicSubscriber.getHumidity());
-    message.put("temperature", topicSubscriber.getTemperature());
-    message.put("dustDensityug", topicSubscriber.getdustDensityug());
+    message.put("humidity", topicPubSub.getHumidity());
+    message.put("temperature", topicPubSub.getTemperature());
+    message.put("dustDensityug", topicPubSub.getdustDensityug());
     return message;
   }
   
@@ -34,7 +34,7 @@ public class AwsIotServiceImpl implements AwsIotService {
 */
   @Override
   public void publish(String topic, String payload) throws AWSIotException {
-    topicSubscriber.publish(topic, payload);
+    topicPubSub.publish(topic, payload);
   }
 
 }
