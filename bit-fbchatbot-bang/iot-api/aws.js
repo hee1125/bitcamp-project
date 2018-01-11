f97460f7e64f4383396cabd2a3b896bb6090a664
unknown <heeyi1125@nate.com>
2018-01-11 20:06:56 +0900

    '.'

--- bit-fbchatbot-bang/iot-api/aws.js
+++ bit-fbchatbot-bang/iot-api/aws.js
@@ -84,14 +84,12 @@ dev01.on('message', function(topic, payload) {
     var dataObj = payload.toString('utf-8')
     var obj = JSON.parse(dataObj)
     var objmap = new Map();
-        if (objmap.set('sensor', obj.sensor)) {
-            objmap.set('sensor', obj.sensor)
+        objmap.set('sensor', obj.sensor)
             temp_value = objmap.set('temp', obj.temp)
             humi_value = objmap.set('humi', obj.humi)
-        } else if (objmap.set('sensor', obj.sensor)) {
-            objmap.set('sensor', obj.sensor)
+        objmap.set('sensor', obj.sensor))
             dust_value = objmap.set('dust', obj.dust)
-        }
+
     console.log(objmap.temp_value);
 
     console.log('받은 메시지:', obj);
