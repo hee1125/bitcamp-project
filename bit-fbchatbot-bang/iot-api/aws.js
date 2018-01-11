614d55fb056de3d963ed3bedd23d8fd7bd40119e
unknown <heeyi1125@nate.com>
2018-01-11 15:35:31 +0900

    '.'

--- bit-fbchatbot-bang/iot-api/aws.js
+++ bit-fbchatbot-bang/iot-api/aws.js
@@ -52,7 +52,7 @@ dev01.on('message', function(topic, payload) {
     console.log('사서함 메시지 도착');
     console.log('사서함 이름:', topic);
     var dataObj = payload.toString('utf-8')
-    var obj = JSON.parse(dataObj)
+    // var obj = JSON.parse(dataObj)
 
     var objmap = new Map(payload.toString('utf-8'));// obj 맵객체로
 /*
